5d952b1a8044f400c5ede7f9a25e10cc42ded212
slam3085 <slam3085@gmail.com>
2019-12-04 14:16:49 +0300

    day 4 solution (only part 1)

---
+++ day_4.py
@@ -0,0 +1,29 @@
+def is_non_decreasing(sequence):
+    for i in range(1, len(sequence)):
+        if sequence[i] < sequence[i - 1]:
+            return False
+    return True
+
+def has_double(sequence):
+    for i in range(len(sequence)):
+        if sequence[i] == sequence[i - 1]:
+            return True
+    return False
+
+def fits_adjacency_rules(sequence):
+    raise NotImplementedError
+
+def is_correct(password, use_adjacency_rules=False):
+    digits = list(str(password))
+    result = is_non_decreasing(digits) and has_double(digits)
+    if use_adjacency_rules:
+        result = result and fits_adjacency_rules(digits)
+    return result
+
+
+if __name__ == '__main__':
+    pass_range = (353096, 843212 + 1)
+    n_passwords = sum((is_correct(p) for p in range(*pass_range)))
+    print(f'different passwords: {n_passwords}')
+    n_passwords_with_adj_rules = sum((is_correct(p, use_adjacency_rules=True) for p in range(*pass_range)))
+    print(f'different passwords (w/ adjacency rules): {n_passwords_with_adj_rules}')
