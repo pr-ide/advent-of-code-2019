d5e5de513746ad5ff82102be9e11e65a178eee47
Pavel Mametev <pavelmametev@yandex.ru>
2019-12-05 11:13:27 +0500

    day 4 minor refactoring

--- task_04.py
+++ task_04.py
@@ -6,7 +6,7 @@ from left to right the digits never decrease
 from collections import defaultdict
 
 
-def get_digit(number, place):
+def get_digit(number: int, place: int) -> int:
     return (number // 10**place) % 10
 
 
@@ -14,14 +14,14 @@ def brute(start: int, end: int) -> int:
     numbers = []
     for number in range(start, end+1):
         has_decrease = False
-        count = defaultdict(int)
+        count = defaultdict(int)  # count pairs in a number
         for place in range(1, 6):
             digit_1 = get_digit(number, place)
             digit_2 = get_digit(number, place-1)
             if digit_1 > digit_2:
                 has_decrease = True
                 break
-            if not digit_1 ^ digit_2:
+            if digit_1 == digit_2:
                 count[digit_1] += 1
         if not has_decrease and 1 in count.values():
             numbers.append(number)
