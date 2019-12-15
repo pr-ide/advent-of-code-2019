bdfd7967c7cf65f97fbe3c16a2ca847227fd470d
slam3085 <slam3085@gmail.com>
2019-12-15 16:21:58 +0300

    refactor day 14 (or not really)

--- day_14.py
+++ day_14.py
@@ -5,53 +5,55 @@ from collections import defaultdict
 from math import ceil
 
 
-def read_input():
-    reactions = sys.stdin.read().split('\n')
-    dependency_graph = defaultdict(set)
-    quantity_map = {}
-    prod_quantity = {}
-    for r in reactions:
-        _in, _out = r.split(' => ')
-        out_quantity, out_type = _out.split(' ')
-        for item in _in.split(', '):
-            in_quantity, in_type = item.split(' ')
-            dependency_graph[out_type].add(in_type)
-            quantity_map[(in_type, out_type)] = (int(in_quantity), int(out_quantity))
-            prod_quantity[out_type] = int(out_quantity)
-    return dependency_graph, quantity_map, prod_quantity
+class Kitchen:
+    def __init__(self):
+        self.dependency_graph = defaultdict(set)
+        self.quantity_map = {}
+        self.prod_quantity = {}
+        self.leftover = defaultdict(int)
+        self.total_ore_used = 0
+        for reaction in sys.stdin.read().split('\n'):
+            _in, _out = reaction.split(' => ')
+            out_quantity, out_type = _out.split(' ')
+            for item in _in.split(', '):
+                in_quantity, in_type = item.split(' ')
+                self.dependency_graph[out_type].add(in_type)
+                self.quantity_map[(in_type, out_type)] = (int(in_quantity), int(out_quantity))
+                self.prod_quantity[out_type] = int(out_quantity)
+
+    def flush(self):
+        self.leftover = defaultdict(int)
+        self.total_ore_used = 0
+
+    def create(self, material, quantity):
+        if material == 'ORE':
+            self.leftover['ORE'] += quantity
+            self.total_ore_used += quantity
+        else:
+            while self.leftover[material] < quantity:
+                times = ceil((quantity - self.leftover[material]) / self.prod_quantity[material])
+                for ingridient in self.dependency_graph[material]:
+                    self.create(ingridient, times * self.quantity_map[(ingridient, material)][0])
+                enough_materials = True
+                for ingridient in self.dependency_graph[material]:
+                    enough_materials = enough_materials and self.leftover[ingridient] >= times * self.quantity_map[(ingridient, material)][0]
+                if enough_materials:
+                    for ingridient in self.dependency_graph[material]:
+                        self.leftover[ingridient] -= times * self.quantity_map[(ingridient, material)][0]
+                    self.leftover[material] += times * self.quantity_map[(ingridient, material)][1]
 
 def solve():
-    dependency_graph, quantity_map, prod_quantity = read_input()
+    kitchen = Kitchen()
     # part 1
-    leftover, created_total = defaultdict(int), defaultdict(int)
-    
-    def create(material, quantity):
-        if material == 'ORE':
-            leftover['ORE'] += quantity
-            created_total['ORE'] += quantity
-        else:
-            while leftover[material] < quantity:
-                times = ceil((quantity - leftover[material]) / prod_quantity[material])
-                for ingridient in dependency_graph[material]:
-                    create(ingridient, times * quantity_map[(ingridient, material)][0])
-                enough_materials = True
-                for ingridient in dependency_graph[material]:
-                    enough_materials = enough_materials and leftover[ingridient] >= times * quantity_map[(ingridient, material)][0]
-                if enough_materials:
-                    for ingridient in dependency_graph[material]:
-                        leftover[ingridient] -= times * quantity_map[(ingridient, material)][0]
-                    leftover[material] += times * quantity_map[(ingridient, material)][1]
-
-
-    create('FUEL', 1)
-    print(f"part 1: {created_total['ORE']}")
+    kitchen.create('FUEL', 1)
+    print("part 1:", kitchen.total_ore_used)
     # part 2
     lower_bound, upper_bound = 1, 10 ** 9
     while upper_bound - lower_bound > 1:
-        leftover, created_total = defaultdict(int), defaultdict(int)
+        kitchen.flush()
         mid = (lower_bound + upper_bound) // 2
-        create('FUEL', mid)
-        if created_total['ORE'] < 10 ** 12:
+        kitchen.create('FUEL', mid)
+        if kitchen.total_ore_used <= 10 ** 12:
             lower_bound = mid
         else:
             upper_bound = mid
