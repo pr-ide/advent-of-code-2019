4123e1c4251e309871e810a42c62e4483a5e027a
slam3085 <slam3085@gmail.com>
2019-12-17 00:16:54 +0300

    add day 16 part 2

--- day_16.py
+++ day_16.py
@@ -18,14 +18,28 @@ def pattern(pos, iters):
 def fft(sequence, n):
     return numpy.array([abs(numpy.dot(sequence, pattern(i, n))) % 10 for i in range(n)])
 
+def fft_second_half(sequence, n):
+    for i in range(n - 2, -1, -1):
+        sequence[i] += sequence[i + 1]
+    return sequence % 10
+
 def solve():
     original_input = list(map(int, tuple(sys.stdin.read())))
+    # part 1
     sequence = numpy.array(original_input)
     n = len(sequence)
     for _ in tqdm(range(100)):
         sequence = fft(sequence, n)
     print('part 1: ', ''.join(map(str, sequence[:8])))
-    
+    # part 2
+    offset = int(''.join(map(str, original_input[:7])))
+    sequence = numpy.tile(numpy.array(original_input), 10000)[offset:]
+    n = len(sequence)
+    for _ in tqdm(range(100)):
+        sequence = fft_second_half(sequence, n)
+    print('part 2: ', ''.join(map(str, sequence[:8])))
+
+
 
 if __name__ == '__main__':
     solve()
