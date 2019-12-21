167d11b5af12369a2d289802a37961449f5f7a70
slam3085 <slam3085@gmail.com>
2019-12-21 12:12:10 +0300

    refactor KOMPUTAR

---
+++ Komputar.py
@@ -0,0 +1,115 @@
+from collections import deque
+
+
+class Komputar:
+    def __init__(self, _input):
+        self.memory = _input
+        self.ptr = 0
+        self.opcodes = {
+            1: self.add,
+            2: self.multiply,
+            3: self.cin,
+            4: self.cout,
+            5: self.jump_if_true,
+            6: self.jump_if_false,
+            7: self.less_than,
+            8: self.equals,
+            99: self.halt_now,
+        }
+        self.stdin = deque()
+        self.stdout = deque()
+    
+    @property
+    def HALT(self):
+        return 1
+
+    @property
+    def PAUSE(self):
+        return 2
+
+    def parse_instruction(self, instruction):
+        opcode = instruction % 100
+        instruction = instruction // 10
+        modes = []
+        for _ in range(3):
+            instruction = instruction // 10
+            modes.append(instruction % 10)
+        return modes, opcode
+
+    def read_memory(self, index, mode):
+        if mode == 0:
+            return self.memory[self.memory[index]]
+        elif mode == 1:
+            return self.memory[index]
+
+    def write_memory(self, index, val):
+        self.memory[self.memory[index]] = val
+
+    def add(self, modes):
+        a = self.read_memory(self.ptr + 1, modes[0])
+        b = self.read_memory(self.ptr + 2, modes[1])
+        self.write_memory(self.ptr + 3, a + b)
+        self.ptr += 4
+        return 0
+
+    def multiply(self, modes):
+        a = self.read_memory(self.ptr + 1, modes[0])
+        b = self.read_memory(self.ptr + 2, modes[1])
+        self.write_memory(self.ptr + 3, a * b)
+        self.ptr += 4
+        return 0
+
+    def cin(self, modes):
+        if self.stdin:
+            value = self.stdin.popleft()
+            self.write_memory(self.ptr + 1, value)
+            self.ptr += 2
+            return 0
+        else:
+            return self.PAUSE
+
+    def cout(self, modes):
+        value = self.read_memory(self.ptr + 1, modes[0])
+        self.stdout.append(value)
+        self.ptr += 2
+        return 0
+
+    def jump_if_true(self, modes):
+        if self.read_memory(self.ptr + 1, modes[0]):
+            self.ptr = self.read_memory(self.ptr + 2, modes[1])
+        else:
+            self.ptr += 3
+        return 0
+
+    def jump_if_false(self, modes):
+        if self.read_memory(self.ptr + 1, modes[0]) == 0:
+            self.ptr = self.read_memory(self.ptr + 2, modes[1])
+        else:
+            self.ptr += 3
+        return 0
+
+    def less_than(self, modes):
+        if self.read_memory(self.ptr + 1, modes[0]) < self.read_memory(self.ptr + 2, modes[1]):
+            self.write_memory(self.ptr + 3, 1)
+        else:
+            self.write_memory(self.ptr + 3, 0)
+        self.ptr += 4
+        return 0
+
+    def equals(self, modes):
+        if self.read_memory(self.ptr + 1, modes[0]) == self.read_memory(self.ptr + 2, modes[1]):
+            self.write_memory(self.ptr + 3, 1)
+        else:
+            self.write_memory(self.ptr + 3, 0)
+        self.ptr += 4
+        return 0
+    
+    def halt_now(self, modes):
+        return self.HALT
+
+    def execute(self):
+        while True:
+            modes, opcode = self.parse_instruction(self.memory[self.ptr])
+            return_code = self.opcodes[opcode](modes)
+            if return_code:
+                return return_code
--- day_5.py
+++ day_5.py
@@ -1,110 +1,19 @@
 import sys
-
-
-class Komputar:
-    def __init__(self, _input):
-        self.memory = _input
-        self.ptr = 0
-        self.opcodes = {
-            1: self.opcode_1,
-            2: self.opcode_2,
-            3: self.opcode_3,
-            4: self.opcode_4,
-            5: self.opcode_5,
-            6: self.opcode_6,
-            7: self.opcode_7,
-            8: self.opcode_8,
-            99: self.opcode_99,
-        }
-
-    def parse_instruction(self, instruction):
-        opcode = instruction % 100
-        instruction = instruction // 10
-        modes = []
-        for _ in range(3):
-            instruction = instruction // 10
-            modes.append(instruction % 10)
-        return modes, opcode
-
-    def read_memory(self, index, mode):
-        if mode == 0:
-            return self.memory[self.memory[index]]
-        elif mode == 1:
-            return self.memory[index]
-
-    def write_memory(self, index, val):
-        self.memory[self.memory[index]] = val
-
-    def opcode_1(self, modes):
-        a = self.read_memory(self.ptr + 1, modes[0])
-        b = self.read_memory(self.ptr + 2, modes[1])
-        self.write_memory(self.ptr + 3, a + b)
-        self.ptr += 4
-        return 0
-
-    def opcode_2(self, modes):
-        a = self.read_memory(self.ptr + 1, modes[0])
-        b = self.read_memory(self.ptr + 2, modes[1])
-        self.write_memory(self.ptr + 3, a * b)
-        self.ptr += 4
-        return 0
-
-    def opcode_3(self, modes):
-        value = int(input())
-        self.write_memory(self.ptr + 1, value)
-        self.ptr += 2
-        return 0
-
-    def opcode_4(self, modes):
-        value = self.read_memory(self.ptr + 1, modes[0])
-        print(value)
-        self.ptr += 2
-        return 0
-
-    def opcode_5(self, modes):
-        if self.read_memory(self.ptr + 1, modes[0]):
-            self.ptr = self.read_memory(self.ptr + 2, modes[1])
-        else:
-            self.ptr += 3
-        return 0
-
-    def opcode_6(self, modes):
-        if self.read_memory(self.ptr + 1, modes[0]) == 0:
-            self.ptr = self.read_memory(self.ptr + 2, modes[1])
-        else:
-            self.ptr += 3
-        return 0
-
-    def opcode_7(self, modes):
-        if self.read_memory(self.ptr + 1, modes[0]) < self.read_memory(self.ptr + 2, modes[1]):
-            self.write_memory(self.ptr + 3, 1)
-        else:
-            self.write_memory(self.ptr + 3, 0)
-        self.ptr += 4
-        return 0
-
-    def opcode_8(self, modes):
-        if self.read_memory(self.ptr + 1, modes[0]) == self.read_memory(self.ptr + 2, modes[1]):
-            self.write_memory(self.ptr + 3, 1)
-        else:
-            self.write_memory(self.ptr + 3, 0)
-        self.ptr += 4
-        return 0
-    
-    def opcode_99(self, modes):
-        return 1
-
-    def execute(self):
-        while True:
-            modes, opcode = self.parse_instruction(self.memory[self.ptr])
-            if self.opcodes[opcode](modes):
-                break
+from Komputar import Komputar
 
 
 def solve():
     _input = list(map(int, sys.stdin.readline().split(',')))
-    komp = Komputar(_input)
+    # part 1
+    komp = Komputar(_input.copy())
+    komp.stdin.append(1)
     komp.execute()
+    print('part 1 ', komp.stdout.pop())
+    # part 2
+    komp = Komputar(_input.copy())
+    komp.stdin.append(5)
+    komp.execute()
+    print('part 2 ', komp.stdout.pop())
 
 
 if __name__ == '__main__':
