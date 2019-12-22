e4b2bcdd20ff8b8cd4aae2db6714337fe18faaec
slam3085 <slam3085@gmail.com>
2019-12-22 18:01:23 +0300

    fix namings from transposed to normal

--- day_19.py
+++ day_19.py
@@ -6,34 +6,34 @@ from functools import lru_cache
 def solve():
     _input = list(map(int, sys.stdin.readline().split(',')))
 
-    def deploy_drone(i, j):
+    def deploy_drone(x, y):
         komp = Komputar(_input.copy())
-        komp.stdin.append(i)
-        komp.stdin.append(j)
+        komp.stdin.append(x)
+        komp.stdin.append(y)
         komp.execute()
         return komp.stdout.popleft()
 
     def part_1():
         affected_area = 0
-        for i in tqdm(range(50)):
-            for j in range(50):
-                affected_area += deploy_drone(i, j)
+        for x in tqdm(range(50)):
+            for y in range(50):
+                affected_area += deploy_drone(x, y)
         return affected_area
     
     def part_2():
 
-        def linear_search(start_i, stop_i):
-            j = 1
-            for i in tqdm(range(start_i, stop_i)):
-                while deploy_drone(i, j - 1):
-                    j -= 1
-                while not deploy_drone(i, j):
-                    j += 1
-                if deploy_drone(i - 99, j) and deploy_drone(i, j + 99) and deploy_drone(i - 99, j + 99):
-                    return (i - 99, j)
+        def linear_search(start_y, stop_y):
+            x = 1
+            for y in tqdm(range(start_y, stop_y)):
+                while deploy_drone(x - 1, y):
+                    x -= 1
+                while not deploy_drone(x, y):
+                    x += 1
+                if deploy_drone(x, y - 99) and deploy_drone(x + 99, y) and deploy_drone(x + 99, y - 99):
+                    return (x, y - 99)
         
-        i, j = linear_search(500, 1500)
-        return i * 10000 + j
+        x, y = linear_search(1500, 2000)
+        return x * 10000 + y
 
     print('part 1:', part_1())
     print('part 2:', part_2())
